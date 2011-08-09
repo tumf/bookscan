b8e65442bb5fffc224ca10704703a8ff59de731f
Yoshihiro TAKAHARA <y.takahara@gmail.com>
2011-08-09 12:52:34 +0900

    update test

--- test/test_bookscan.rb
+++ test/test_bookscan.rb
@@ -5,10 +5,8 @@ require 'pp'
 class TestBookscan < Test::Unit::TestCase
   context "Bookscan::Agent" do
     should "access to bookscan" do
-      Bookscan::Agent.new {|agent|
-        agent.getr("/")
-        assert(/ブックスキャン/ =~ agent.page.body,"access to top")
-      }
+      agent = Bookscan::Agent.new
+      agent.getr("/")
     end
   end
 end
